ab74ed43b9a8291f1893881e527a7d7f266be646
Alexandra Ogarok <alexandra.ogarok@gmail.com>
2019-07-26 22:15:18 -0700

    Add Babel test

---
+++ 1-Babel-test-setup/src/main.js
@@ -0,0 +1,8 @@
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
